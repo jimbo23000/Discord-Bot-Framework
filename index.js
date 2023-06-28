9fc56cbdeb83ca031d1232993e8ae1f24383d854
jimbo23000 <antonio.ramos23@yahoo.com>
2023-06-28 18:05:59 -0500

    Added a data storage package

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('database', 'user', 'password', {
+	host: 'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	storage: 'database.sqlite',
+});
