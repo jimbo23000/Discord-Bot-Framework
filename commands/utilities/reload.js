227a4a0b15c442c254fd848133fed81367999edd
jimbo23000 <antonio.ramos23@yahoo.com>
2023-07-06 13:55:23 -0500

    Updated syntax

--- commands/utilities/reload.js
+++ commands/utilities/reload.js
@@ -3,10 +3,10 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('reload')
-		.setDescription('Reloads a command.')
+		.setDescription('Reloads a command')
 		.addStringOption(option =>
 			option.setName('command')
-				.setDescription('The command to reload.')
+				.setDescription('The command to reload')
 				.setRequired(true)),
 	async execute(interaction) {
 		const commandName = interaction.options.getString('command', true).toLowerCase();
