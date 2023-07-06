fc4cf71a70f1ad1610bc8cbb41922a642419c61b
jimbo23000 <antonio.ramos23@yahoo.com>
2023-07-06 13:03:21 -0500

    Updated clientReady for database implementation

--- events/clientReady.js
+++ events/clientReady.js
@@ -1,9 +1,16 @@
 const { Events } = require('discord.js');
+// Database synchronization implementation
+const { Users } = require('./dbObjects.js');
+const { currency } = require('./helpers/addBalance');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+		// Database synchronization implementation
+		const storedBalances = await Users.findAll();
+		storedBalances.forEach(i => currency.set(i.user_id, i));
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
